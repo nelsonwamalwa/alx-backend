1eaa15e93f63c0cd63c0b30ba96d786c00e4a4fd
NelsonWamalwa <wamalwanelson@gmail.com>
2024-05-14 23:56:26 +0300

    Tasks With Babel file

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,11 @@
 """
 Flask app
 """
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
 
@@ -20,12 +24,20 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
     Handles / route
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
